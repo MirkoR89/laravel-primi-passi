94fcced7d0160891ed53e1693f9810817591e740
Mirko <mirko.rinelli@gmail.com>
2021-02-10 17:25:49 +0100

    Working on sass

--- blog/resources/views/header.blade.php
+++ blog/resources/views/header.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel primi passi</title>
-</head>
-<body>
-    
+<!-- Header site -->
+<header>
+    <!-- Navebar -->
+    <nav>
+        <div class="container">
+            <ul class="d-flex align-items-center ">
+                <li><a href="">Home</a></li>
+                <li><a href="">About me</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
 </body>
-</html>
