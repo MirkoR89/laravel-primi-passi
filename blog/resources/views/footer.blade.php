97be9bd24a598231a5d60237f62fa89b1f97e7dc
Mirko <mirko.rinelli@gmail.com>
2021-02-10 18:02:49 +0100

    Header and footer completed

---
+++ blog/resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-around">
+            <ul>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+            </ul>
+        </div>
+
+    </div>
+</footer>
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
     </head>
     <body>
         @include('header')
+        @include('main')
         @include('footer')
     </body>
 </html>
